a4a769bd858ae56696993868d74f8fce190d7000
Elisabetta Fino <elisabetta.fino@gmail.com>
2025-12-30 10:49:05 +0100

    Add initial menu and JSON structure for course and participant management

---
+++ main.py
@@ -0,0 +1,45 @@
+'''
+Menu
+
+1. Corsi:
+    a. Lista corsi esistenti
+    b. Aggiungi corso
+    c. Elimina corso
+
+2. Partecipanti:
+    a. Lista partecipanti
+        i. Nome
+        ii. Cognome
+        iii. Corso che segue
+    b. Iscrivere un nuovo partecipante
+    c. Rimuovere un partecipante
+
+3. Assegnazione goleador:
+    a. Quante goleador sono state vinte?
+    b. Chi le ha vinte?
+    c. Quando sono state vinte? Date and time - timestamp
+
+4. Analytics / Statistiche:
+    a. Totale goleador vinte per ogni corso
+    b. Top scorer di ogni corso
+    c. Identificare la 'Top Scorer
+'''
+
+
+# JSON FILE
+''' {
+        "corso_nome": "Python junior",
+        "partecipanti": [
+            {
+            "nome" : "Anna",
+            "cognome" : "Rossi",
+            "goleadore" : "x"
+            },
+            {
+            "nome" : "Mario",
+            "cognome" : "Bianchi",
+            "goleadore" : "y"
+            } 
+            ]
+    }
+'''
